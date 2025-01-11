f2e137bcbe35e7b562f7edc483f2fabc24b0176f
supikashi <147439314+supikashi@users.noreply.github.com>
2025-01-11 23:18:12 +0700

    lab1 toast

--- lab1/app/src/main/java/com/example/lab1/MainActivity.kt
+++ lab1/app/src/main/java/com/example/lab1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lab1
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,6 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        Toast.makeText(applicationContext, "toast", Toast.LENGTH_SHORT).show()
     }
 }
