4b2d0e967e63270ad23473a395a9df7df998ae97
supikashi <147439314+supikashi@users.noreply.github.com>
2025-01-14 14:16:03 +0300

    lab3

---
+++ lab3/app/src/main/java/com/example/lab3/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.lab3
+
+import android.app.Application
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val button = findViewById<Button>(R.id.next_button)
+        val editText = findViewById<EditText>(R.id.edit_text)
+        button.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java)
+            val string = editText.text
+            intent.putExtra("string", string.toString())
+            startActivity(intent)
+        }
+    }
+}
---
+++ lab3/app/src/main/java/com/example/lab3/SecondActivity.kt
@@ -0,0 +1,33 @@
+package com.example.lab3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_second)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val string = intent.getStringExtra("string")
+        val textView = findViewById<TextView>(R.id.textView)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            finish()
+        }
+        textView.text = string
+
+    }
+}
